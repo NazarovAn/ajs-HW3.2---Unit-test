149f360ea6805a3724bf0288a5d188f2f22db717
NazarovAn <naatonchrome@gmail.com>
2020-04-30 14:29:14 +0400

    small fix index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import sortByHealth from './js/app';
 import { renderList, renderListAfterSort } from './js/renderList';
 
 const charactersArray = [
-  { name: 'мечник', health: 10 },
+  { name: 'мечник', health: 15 },
   { name: 'маг', health: 100 },
   { name: 'лучник', health: 80 },
   { name: 'рыцарь', health: 90 },
@@ -13,6 +13,10 @@ const charactersArray = [
   { name: 'мечник', health: 60 },
 ];
 
-renderList(charactersArray);
-sortByHealth(charactersArray);
-renderListAfterSort(charactersArray);
+function sortAndRender(data) {
+  renderList(data);
+  sortByHealth(data);
+  renderListAfterSort(data);
+}
+
+sortAndRender(charactersArray);
